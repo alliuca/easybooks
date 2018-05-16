9b1258d57b6fa0ea2e7fe235336fb535a2f1de25
Luca Allievi <alliuka@gmail.com>
2018-05-16 17:48:24 +0200

    add babel-plugin-emotion for source maps during development

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,6 @@ const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override (config, env) {
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+  config = injectBabelPlugin(['emotion', { sourceMap: env === 'development' ? true : false }], config);
   return config;
 };
