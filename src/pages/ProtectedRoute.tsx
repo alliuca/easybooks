1453edece904a1d9f7a744b6d8b589bf061dc57d
Luca Allievi <alliuka@gmail.com>
2019-07-01 15:05:57 +0200

    Add ProtectedRoute.tsx

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Cookies from 'js-cookie';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
 
-class ProtectedRoute extends Component {
-  isAuthenticated = () => {
-    return Cookies.get('EasyBooksToken') || false;
-  };
+interface Props extends RouteProps {
+  component: React.ComponentClass;
+}
+
+class ProtectedRoute extends Component<Props> {
+  isAuthenticated = (): string | false => Cookies.get('EasyBooksToken') || false;
 
   render() {
     const { component: WrappedComponent, ...rest } = this.props;
