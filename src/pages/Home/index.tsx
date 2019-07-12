5c9dfc2e7b9d87dbfe0d4a4d74c58be13853fd89
Luca Allievi <alliuka@gmail.com>
2019-07-12 22:17:32 +0200

    Change Home text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from 'components/Layout';
 
-const Home: React.FunctionComponent = () => <Layout>Content</Layout>;
+const Home: React.FunctionComponent = () => <Layout>Thanks for using EasyBooks!</Layout>;
 
 export default Home;
